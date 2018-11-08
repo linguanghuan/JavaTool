7e4426b3ddab1afeea846bee73dabab425615c73
linguanghuan <linguanghuan@gmail.com>
2018-11-08 14:44:05 +0800

    Update IncrementalPackage.java
    参数支持

--- src/main/java/maven/IncrementalPackage.java
+++ src/main/java/maven/IncrementalPackage.java
@@ -19,7 +19,7 @@ public class IncrementalPackage {
             + File.separator + "repository";
 
     // 默认从当前时间的24小时前开始增量打包
-    private static final int DEFAULT_START = 48;
+    private static final int DEFAULT_START = 24;
 
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
@@ -27,20 +27,24 @@ public class IncrementalPackage {
             + File.separator + ".m2" + File.separator + "output.zip";
 
     public static void main(String[] args) throws IOException {
+        int startHour = DEFAULT_START;
+        String location = REPOSITORY_DEFAULT_LOCATION;
+        if (args.length == 2) {
+            startHour = Integer.valueOf(args[0]);
+            location = args[1];
+        }
         long begin = System.currentTimeMillis();
-        System.out.println("增量打包");
-        System.out.println(REPOSITORY_DEFAULT_LOCATION);
-        Date startPoint = new Date(new Date().getTime() - DEFAULT_START * 3600 * 1000);
-        System.out.println("start point:" + startPoint);
+        Date startPoint = new Date(new Date().getTime() - startHour * 3600 * 1000);
+        System.out.println("增量打包, 位置:"+ location + ", 开始时间：" + startPoint);
         Set<String> pathToPack = new HashSet<String>();
-        File startDir = new File(REPOSITORY_DEFAULT_LOCATION);
+        File startDir = new File(location);
         walkForMavenFiles(startDir, startPoint, pathToPack);
         System.out.println("size:" + pathToPack.size());
         ZipCompressor zc = new ZipCompressor(OUTPUT_FILE);
         Set<String> ignoreSuffixSet = new HashSet<String>();
         ignoreSuffixSet.add(".repositories");
         ignoreSuffixSet.add(".lastUpdated");
-        zc.compress(pathToPack, REPOSITORY_DEFAULT_LOCATION.replace("repository", ""), ignoreSuffixSet);
+        zc.compress(pathToPack, location.replace("repository", ""), ignoreSuffixSet);
         System.out.println("elapse:" + (System.currentTimeMillis() - begin));
     }
 
